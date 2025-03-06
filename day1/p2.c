21295325179059169df6615330a53a05962ce099
neelmyna <nithincheeta@gmail.com>
2025-03-06 15:34:02 +0530

    asessment link and feedback link added in notes file

--- day1/p2.c
+++ day1/p2.c
@@ -3,12 +3,8 @@
 
 int main()
 {
-    int num1 = 0;
-    long int num2 = 0;
-    long long int num3 = 0;
-    double d = 0.0;
-    float f = 0.0f;
-    printf("%d  %d  %d  %d  %d \n", sizeof(num1), sizeof(num2), sizeof(num3), sizeof(d), sizeof(f));
-    printf("%d  %d  %d  %d  %d", sizeof(int), sizeof(long int), sizeof(long long int), sizeof(double), sizeof(float));
+    int i = -1;
+    i -= -1;
+    printf("%d", i);
     return 0;
 }
