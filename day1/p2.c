a931e972eb86e12193e172ae084328d752559ba5
neelmyna <nithincheeta@gmail.com>
2025-03-07 13:17:46 +0530

    session2 of day2 notes added

--- day1/p2.c
+++ day1/p2.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 
+int main()
+{
+    int inputNumber = 0;
+    puts("Enter a number to print its Math table");
+    scanf("%d", &inputNumber);
+    for (int i = 1; i <= 20; i++)
+    {
+        printf("%d * %02d  %03d \n", inputNumber, i, inputNumber * i);
+    }
+}
