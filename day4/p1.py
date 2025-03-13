2ca6a4375f37a68ecee7175ca5fb33795e9afaac
neelmyna <nithincheeta@gmail.com>
2025-03-13 10:50:43 +0530

    day4 session1 programs added

---
+++ day4/p1.py
@@ -0,0 +1,2 @@
+num = 25
+print('Value of num = ', num)
---
+++ day4/p2.py
@@ -0,0 +1,9 @@
+# Accept a number and find its square root
+import math
+
+print('Enter a number to find its Root')
+input_number = int(input())
+
+root_number = math.sqrt(input_number)
+#print('Square Root of', input_number, ' is', root_number)
+print('Square Root of', input_number, ' is ' + str(root_number))
